1a0e83eed1b6bc49209d97c0b896571adb39a6c1
Pineson86 <andreysosnoff@gmail.com>
2024-11-08 12:03:53 +0300

    MHome work #1 modified a littl

--- Задание 1 от 30.10.2024/task1_linear_equations_solving.js
+++ Задание 1 от 30.10.2024/task1_linear_equations_solving.js
@@ -1,19 +1,17 @@
 // Решение системы линейных уравнений методом Крамера
 // Коэффициенты
-const n1 = 2;
-const n2 = 4;
-const n3 = 6;
-const n4 = 8;
-const n5 = 10;
-const n6 = 12;
+const [, , n1, n2, n3, n4, n5, n6] = process.argv.map(Number); //преобразуем все переданные значения в числа
+if ([n1, n2, n3, n4, n5, n6].some(isNaN)) {
+    console.log('Ошика! Все переданные значения должны быть числами');
+} else{
+    const mainDet = n1 * n5 - n2 * n4; //Основной определитель
 
-const mainDet = n1 * n5 - n2 * n4; //Основной определитель
+    const xDet = n3 * n5 - n2* n6; //Определитель для x
 
-const xDet = n3 * n5 - n2* n6; //Определитель для x
+    const yDet = n1 * n6 - n3 * n4; // Определитель для y
 
-const yDet = n1 * n6 - n3 * n4; // Определитель для y
+    const x = xDet / mainDet;
+    const  y = yDet / mainDet;
 
-const x = xDet / mainDet;
-const  y = yDet / mainDet;
-
-mainDet === 0 ? console.log('Система не имеет единственного решения') : console.log('x = ', x, 'y = ', y);
+    mainDet === 0 ? console.log('У системы бесконечное множество решений') : console.log(`${x} ${y}`);
+}
